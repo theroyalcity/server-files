36f12b1ca125f5fc1bd921e00c0caa1d0556192a
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-05-24 06:40:37 +0300

    ATM config.lua

---
+++ code/ATM config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Models = {'prop_atm_01', 'prop_atm_02', 'prop_fleeca_atm', 'prop_atm_03'}
+
+Config.EnableBanks = true -- Whetever or not you want the UI to be accessible from banks
+Config.EnableBankBlips = true
+
+Config.Banks = {
+    {x = -113.43, y = 6469.88, z = 31.63, h = 317.91},
+    {x = 1175.19, y = 2706.48 z = 38.09, h = 1.47},
+    {x = 150.03, y = -1040.42, z = 29.37, h = 160.89},
+    {x = 241.25, y = 224.63, z = 106.29, h = 342.93},
