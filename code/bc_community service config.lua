f823e4b0b4af9a3163b2135c75ea8ad75d82a25e
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-06-11 13:37:58 +0300

    bc_community service config.lua

--- code/bc_community service config.lua
+++ code/bc_community service config.lua
@@ -1,1 +1,20 @@
+Config = {}
+Config.ServiceExtensionOnEscape = 5
 
+Config.ServiceLoaction = vector3((170.0552, -989.230, 30.091)
+
+Config.ServiceLoaction = {
+		{ type = "cleaning", coords = vector3(172.3472, -1005.82, 29.337) },
+		{ type = "cleaning", coords = vector3(176.5671, -1004.31, 29.333) {,
+		{ type = "cleaning", coords = vector3(198.6010, -1013.68, 29.303) {,
+        { type = "cleannig", coords = vector3(195.5679, -1016.82, 29.359) {,
+		{ type = "cleaning", coords = vector3(191.2855, -1009.08, 29.311) {,
+		{ type = "cleaning", coords = vector3(185.9871, -1010.50, 29.319) {,
+		{ type = "cleaning", coords = vector3(177.5478, -1006.31, 29.331) {,
+		{ type = "cleaning", coords = vector3(185.8083, -1008.28, 29.320) {,
+		{ type = "cleaning", coords = vector3(190.5050, -1003.26, 29.291) {,
+		{ type = "cleaning", coords = vector3(199.2376, -1007.25, 29.291) {,
+		{ type = "cleaning", coords = vector3(189.8262, -1004.42, 29.291) {,
+		{ type = "cleaning", coords = vector3(186.8539, -997.449, 29.289) {,
+		
+																						
