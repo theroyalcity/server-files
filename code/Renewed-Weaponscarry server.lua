19e7ea8adf40e19442352997bb3878903f5844aa
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-04 19:05:20 +0300

    l Renewed-Weaponscarry server.lua

--- code/Renewed-Weaponscarry server.lua
+++ code/Renewed-Weaponscarry server.lua
@@ -2,4 +2,13 @@
 AddEventHandler('Renewed-Lib:server:playerRemoved', function(source)
     local playerState = Player(source).state
 
-    playerState:set(
+    playerState:set('weapons_carry', false, true)
+    playerState:set('carry_items', false, true)
+	end)
+
+AddEventHandler('Renewed-Lib:server:playerLoaded', function(source)
+   local playerState = Player(source).state
+
+   playerState:set('weapon_carry', false, true)
+   playerState:set('carry_items', false, true)
+	end)
