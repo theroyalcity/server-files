9d2cbc75b8e0a0759f076b62d91083a8521d743f
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-04 16:10:36 +0300

    xsound config.lua

--- code/xsound config.lua
+++ code/xsound config.lua
@@ -15,4 +15,9 @@ config.distanceBeforeUpdatingPos = 40
 -- Message list
 config.Messages = {
     ["streamer_on"] = "Streamer mode is on. From. now you will not hear any music/sound.",
-    ["streamer_off"] = "Streamer mode is off. From now you will be able to listen to music players
+    ["streamer_off"] = "Streamer mode is off. From now you will be able to listen to music players might play.",
+
+	["no_permission"] = "You cant use this command, you dont have premission for it!",
+}
+
+--
