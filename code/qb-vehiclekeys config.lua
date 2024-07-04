f968ec46e7bbbb72e237dc5e501f2c296509eb0a
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-04 19:54:39 +0300

    qb-vehiclekeys

--- code/qb-vehiclekeys config.lua
+++ code/qb-vehiclekeys config.lua
@@ -2,4 +2,6 @@ Config = {}
 
 -- Vehicle lock settings
 Config.LockToggleAnimation = {
-    AnimDict = 'anim@p_player
+    AnimDict = 'anim@p_player_intmenu@key_fob@',
+    Anim = 'fob_click'
+    prop = 'prop_cuff_keys_01',
