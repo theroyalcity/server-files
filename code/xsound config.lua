d3ffaf5f5dda00c2836cc3d2238c2bb1d11a2fdd
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-04 16:07:04 +0300

    xsound config.lua

---
+++ code/xsound config.lua
@@ -0,0 +1,18 @@
+config = {}
+
+-- How much ofter the player position is updated ?
+config.RefreashTime = 300
+
+-- default sound format for interact
+config.interact_sound_file = "ogg"
+
+-- is emulator enabled ?
+config.interact_sound_enable = fales
+
+-- how much close player has to be to the sound before starting updating postion ?
+config.distanceBeforeUpdatingPos = 40
+
+-- Message list
+config.Messages = {
+    ["streamer_on"] = "Streamer mode is on. From. now you will not hear any music/sound.",
+    ["streamer_off"] = "Streamer mode is off. From now you will be able to listen to music players
