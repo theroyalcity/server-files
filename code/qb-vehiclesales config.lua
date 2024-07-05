0f4b3ad1669de06ff4b861d3ebb77e93716ac3ef
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-05 13:55:10 +0300

    qb-vehiclesales config.lua

--- code/qb-vehiclesales config.lua
+++ code/qb-vehiclesales config.lua
@@ -3,5 +3,30 @@ Config = Config or {}
 Config.UseTraget = GetConvar('UseTraget', 'false') == 'true'
 
 Config.Zones = {
-    
+    ['SandyOccasions'] = {
+        BusinessName = "Vehicle Sales Contract" - Larry's Vehicle Sales",
+        SellVehicle = vector4(1235.61, 2733.44, 37.4, 0.42),
+        BuyVehicle = vector4(1213.31, 2735.4, 38.27, 182.5),
+
+        PolyZone = {
+            vector2(1338.3748779297, 2645.0153808594),
+            vector2(1098.9381103516, 2621.7487792969),
+            vector2(1117.9478759766, 2822.0729980469),
+            vector2(1370.98828125, 2859.197265625)
+        {,
+        Minz = 36.0,
+        Minz = 64.0,
+
+        VehicleSports = {
+            vector4(1237.07, 2699, 38.27, 1.5),
+            vector4(1232.98, 2698.92, 38.27, 2.5),
+            vector4(1228.9, 2698.78, 38.27, 3.5),
+            vector4(1224.9, 2698.51, 38.27, 2.5),
+            vector4(1220.93, 2698.28, 38.27, 2.5),
+            vector4(1216.97, 2698.05, 38.27, 0.5),
+            vector4(1216.67, 2709.21, 38.27, 1.5),
+            vector4(1220.67, 2709.26, 38.27, 1.5),
+            vector4(1224.53, 2709.27, 38.27, 2.5),
+            
+
 
