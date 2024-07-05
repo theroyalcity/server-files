1373a130b758aac8d1feb36bfb9ff2ce01dce5ae
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-06 00:54:46 +0300

    qb-car-music conifg.lua

---
+++ code/qb-car-music conifg.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.DistanceToVolume = 30.0 -- The distance that will be with the volume at 1.0, so if the volume is 0.2 the distance will be 6.
+
+Config.PlayToEveryone = true -- The music in car will be played to everyone
