f775e430143d2bedb2528502ff37f0c476b5f63b
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-07-12 21:22:55 +0300

    v-vehControl config.lua

---
+++ code/v-vehControl config.lua
@@ -0,0 +1,12 @@
+Config = {
+    Debug = true,
+    CommandName = "vehmenu",
+    Keybind = {
+        enabled = true,
+        key = "B" -- The Key to opening the menu if enabled
+  },
+  Functions = {
+      HasKeys = function()
+          return true
+    end
+  }
