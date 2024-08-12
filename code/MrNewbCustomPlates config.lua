29c1be9d40a26263eaa75d6f01320731cf545eeb
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-08-13 00:13:22 +0300

    MrNewbCustomPlates config.lua

---
+++ code/MrNewbCustomPlates config.lua
@@ -0,0 +1,17 @@
+Config = {
+        Debug = false,
+        CustomPlateName = "customizeableplate",
+        Logs = false,
+        Notify = "ox",
+        Input = "ox",
+        AltKeys = false,
+        Keys = "mrnewb",
+        Icon = 'fa-solid fa-car-side',
+        PlateLengthText = "Must Be 8 Characters Long",
+        MinNumbers = 8,
+        MaxNumbers = 8,
+        FilteredWords = {
+
+  }
+}
+        
