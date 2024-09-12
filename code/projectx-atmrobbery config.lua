920b23bbf1b3cc9836d18109bb38e41c967f4429
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-09-12 13:46:55 +0300

    projectx-atmrobbery config.lua

--- code/projectx-atmrobbery config.lua
+++ code/projectx-atmrobbery config.lua
@@ -1,1 +1,24 @@
-
+return {
+    Debug = false,
+    Framework = "cfx default",
+    Police = {
+        jobs = {
+            "police"
+        }
+    },
+    MinimumPolice = 1,
+    Dispatch = 'ps-dispatch', -- If using qb-dispatch otherwise add your export in client policeCall function
+    RequiredItem = 'atmhacker',
+    InventoryType = 'ox_inventory',
+    LabelIcon = 'fa-solid fa-user-ninja',
+    ProgressDuration = 7500,
+    Emotes = 'rpemotes', -- If using rpemotes otherwise anything else for scully_emotemenu
+    PossibleTerritories = true, -- Integrates my paid territrory script - https://possible-scripts.tebex.io/package/6045013 set false if no likey
+    PossibleGangLevel = true, -- Integrates my paid gang level script - https://possible-scripts.tebex.io/package/6036883 set false if no likey
+    Cash = true,
+    CashItem = 'cash',
+    DirtyCashType = 'markedbills', -- If using qb-inventory and cahs = false
+    MinReward = 500, 
+    MaxReward = 2000,
+    NotifPosition = 'top'
+}
