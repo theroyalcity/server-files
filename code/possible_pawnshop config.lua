4208a3851d2023e7c8ebf1b6d4656e5f3b635cb6
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-09-23 16:07:49 +0300

    possible_pawnshop config.lua

---
+++ code/possible_pawnshop config.lua
@@ -0,0 +1,38 @@
+return {
+    Debug = false,
+    sphereZones = {
+        {
+            id = 1,
+            coords = vector3(412.21, 315.15, 103.13),
+            radius = 3.0, 
+        },
+        {
+            id = 2,
+            coords = vector3(182.39, -1319.46, 29.32),
+            radius = 3.0, 
+        }
+    },
+    shopBlips = {
+        {
+            label = 'Pawnshop North',
+            sprite = 431,
+            color = 5,
+            coords = vector3(412.21, 315.15, 103.13),
+            shortRange = true,
+        },
+        {
+            label = 'Pawnshop South',
+            sprite = 431,
+            color = 5,
+            coords = vector3(182.39, -1319.46, 29.32),
+            shortRange = true,
+        }
+    },
+    sellItems = {
+        PawnShop = {
+            goldingot = 750,
+            goldflake = 99,
+            ruby_ring = 350,
+        }
+    }
+}
