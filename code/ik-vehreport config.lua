39533b32eff0e1f698f5312c746d645a8004c0ba
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-09-24 13:32:53 +0300

    ik-vehreport config.lua

---
+++ code/ik-vehreport config.lua
@@ -0,0 +1,14 @@
+Config = Config or {}
+
+Config.InspectionItemName = "car-report"
+
+Config.NearByListRadius = 5
+
+Config.ShowNos = false
+
+Config.JobsOnly = true
+Config.AllowedJobs = {
+    "mechanic",
+    "bennys",
+}
+Config.SendMoneyToFunds = true
