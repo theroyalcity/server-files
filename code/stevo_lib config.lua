c6febae1b7f70d6837a1936bdaeaeffecd6625a2
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-10-20 20:17:25 +0300

    Create stevo_lib config.lua

---
+++ code/stevo_lib config.lua
@@ -0,0 +1,11 @@
+return {
+
+    useInteract = false, -- if https://github.com/darktrovx/interact should be used where possible
+
+    NotifyType = 'ox_lib', -- Support for 'ox_lib' 'qb' 'esx' 'QBBOX' 'okok' and 'custom' (custom requires editing)
+
+    TextUIType = 'ox_lib', -- Support for 'ox_lib' 'Is_textui' (project Is) and 'custom' (custom requires editing)
+
+    businesses = {
+        logs = false, -- Can be 'discord' or 'oxlogs' Check the documentation for info on installing discord logs.
+   },
