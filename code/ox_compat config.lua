1b17cb196125544963b71a2d0790404df65ecc4f
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-11-10 17:45:43 +0300

    ox_compat config.lua

---
+++ code/ox_compat config.lua
@@ -0,0 +1,13 @@
+QBCore = exports['qb-core']:GetCoreObject()
+Config = {}
+
+Config.Modules = {
+    ['qb-menu'] = {
+        active = true, -- Do you need this module to be enabled?
+        resource_name = 'qb-menu' -- What is the name of the resource that provided this module? (In case you changed its name)
+  },
+  ['qb-input'] = {
+      active = true, -- Do you need this module to be enabled?
+      resource_name = 'qb-input' -- What is the name of the resource that provided this module (In case you changed its name)
+  },
+  ['qb-target']
