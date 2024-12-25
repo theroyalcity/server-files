49819575a80dbabe7d7a388dcd65cc62396aa09f
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-12-25 12:11:38 +0300

    Create ELS-FiveM config.lua

---
+++ code/ELS-FiveM config.lua
@@ -0,0 +1,28 @@
+outputLoading = false
+playButtonPressSounds = true
+printDebugInformation = false
+
+vehicleSyncDistance = 150
+enviornmentLightBrightness = 0.006
+lightDelay = 10 -- Time in MS
+flashDelay = 15
+
+panelEnabled = true
+panelType = "original"
+panelOffsetX = 0.0
+panelOffsetY = 0.0
+
+allowedPanelTypes = {
+    "original"
+    "old"
+}
+
+-- https://docs.fivem.net/game-references/controls
+
+shared = {
+    horn = 86,
+}
+
+keyboard = {
+    modifyKey = 132,
+    stageChange = 85, -- Q
