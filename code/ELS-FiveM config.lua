2e7024d862c3b7cb512001bc3193018b923ec6fd
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-12-25 12:30:32 +0300

    ELS-FiveM config.lua

--- code/ELS-FiveM config.lua
+++ code/ELS-FiveM config.lua
@@ -26,3 +26,31 @@ shared = {
 keyboard = {
     modifyKey = 132,
     stageChange = 85, -- Q
+    guiKey = 199, -- P
+    takedown = 83, -- =
+    siren = {
+        tone_one = 157, -- 1
+        tone_two = 158, -- 2
+        tone_three = 160, -- 3
+    },
+    pattern = {
+        primary = 163, -- 9
+        secondary = 162, -- 8
+        advisor = 161, -- 7
+    },
+    warning = 246, -- Y
+    secondary = 303, -- U
+    primary = 7, -- ??
+}
+
+controller = {
+    modifyKey = 73,
+    stageChange = 80,
+    takedown = 74,
+    siren = {
+        tone_one = 173,
+        tone_two = 85,
+        tone_three = 172,
+    },
+}
+        
