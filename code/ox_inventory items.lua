f53743e5aabe89062fc80067c7f348cec5c9b5de
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2024-12-31 14:05:39 +0300

    ox_inventory items.lua

--- code/ox_inventory items.lua
+++ code/ox_inventory items.lua
@@ -1,1 +1,12 @@
-return
+	['crutch'] = {
+		label = 'Crutch',
+		weight = 165,
+		stack = false,
+		close = true,
+	},
+	['wheelchair'] = {
+		label = 'Wheelchair',
+		weight = 540,
+		stack = false,
+		close = true,
+	},
