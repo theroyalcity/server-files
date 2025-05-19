2eceaaee156a2010d7d218c8ebf99505e3fa48fa
theroyalcity <105167048+theroyalcity@users.noreply.github.com>
2025-05-19 09:29:59 +0300

    bbv_skydive config.lua

---
+++ code/bbv_skydive config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.Debug = false
+
+Config.Settings = {
+        Target = "OX" -- QB/OX/BT
+}
+
+Config.SkyDive = {
+        Locations = {
+                vector4(-928.82, -2543.67, 15.36, 90.26),
+                vector4(1724.85, 3288.67, 41.17, 256.24),
+                vector4(134.28, 6640.36, 31.76, 225.58)
+       }
+}
